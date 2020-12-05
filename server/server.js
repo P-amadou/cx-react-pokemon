2a79c0e8823805841695834d7d1035d711afdaa0
Amadou <amadou_dieye@hotmail.fr>
2020-12-05 19:31:33 +0100

    update folder server

--- server/server.js
+++ server/server.js
@@ -1,27 +1,52 @@
 const express = require('express')
 const {development}= require('../knexfile')
-const fileData=require('./fillData')
+//const fileData=require('./fillData')
 let app = express()
 let port=4242 //process.argv[2]
-
+const router=express.Router()
+const routePokemon=require('./route')
 const knex = require('../knex/knex.js')(development);
-  
+ 
 
-  
-  
 let selectAllPokemon=knex.select().from('pokemon')
-app.get('/',(request,response)=>{
+
+app.get('/',(req,res)=>{
+      res.send(`<h1>Welcome to our API Pokedex </h1>`)
+   })
+
+app.get('/pokemons',(req,res)=>{
   selectAllPokemon 
     .then((data)=>{
-      response.send(data)
+      res.send(data)
     })
-  
-})
-app.post('/',(req,res)=>{
+   })
+
+app.get('/pokemons/:id',(req,res)=>{
+  let {id}=req.params
+  let reg=/:/g
+  id=id.replace(reg,"")
+    console.log(id);
+    let selectAllDataPokemonById=knex.select().from('pokemon').where('idP', id)
+    selectAllDataPokemonById.then((data)=>{
+      res.send(data)
+    })
+  })
+
+app.post('/pokemons',(req,res)=>{
     knex.insert(req.body).returning('*').into('pokemon').then((data)=>{
       res.send(data)
     })
   })
+
+app.delete('/pokemons/:id',(req,res)=>{
+  let {id}=req.params
+  let reg=/:/g
+  id=id.replace(reg,"")
+  let deleteAllDataPokemonById=knex('pokemon').del().where('idP', id)
+  deleteAllDataPokemonById.then((data)=>{
+    res.send(data)
+  })
+})
 
 app.listen(port, () => {
     if (port==undefined||port==null) {
