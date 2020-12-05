dee661a01a63885c00f1292dce8d313fb05d364a
Amadou <amadou_dieye@hotmail.fr>
2020-12-05 11:16:27 +0100

    Revert "ancienne version"
    This reverts commit a194cf8d2ce4fa9147fe81bdb19772c4c2c8a532.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const {development}= require('../knexfile')
 const fileData=require('./fillData')
+const express = require('express')
 let app = express()
 let port=4242 //process.argv[2]
 
-const knex = require('../knex/knex.js')(development);
-  
+const knex = require('../knex/knex.js')({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'Connecter0',
+      database : 'pokedex',
+      charset: 'utf8'
+    }
+  });
 
   //app.use()
   
-let selectAllPokemon=knex.select().from('pokemon')
+selectAllPokemon=knex.select('id').from('pokemon')
 app.get('/',(request,response)=>{
   selectAllPokemon 
     .then((id)=>{
@@ -19,9 +26,8 @@ app.get('/',(request,response)=>{
 })
 
 app.listen(port, () => {
-    if (port==undefined||port==null) {
+    if (port==undefined) {
       console.log(`Syntaxe: node server.js <PORT>`);
-      
     }else{
       console.log(`Server is listening on port ${port}`)
     }
